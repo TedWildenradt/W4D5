c0fcee72e46e78843506cbe86acd45d66e491cd4
TedWildenradt <tedkwildenradt@gmail.com>
2018-11-16 12:12:42 -0800

    Add user model and user controller

---
+++ goal_setter/app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def method_name
+    
+  end
+end
---
+++ goal_setter/spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:email)}
+  it { should validate_presence_of(:password_digest)}
+  it { should validate_length_of(:password).is_at_least(6)} 
+  
+  describe 'session token' do 
+    it 'should validate the presence of a session token' do
+      user = User.create(email: 'dumb@email.com', password: 'password')
+      # debugger
+      expect(user.session_token).to_not be_nil
+    end 
+  end 
+  
+  describe '#find_by_credentials' do
+    it 'should be able to find a user by their email and password' do 
+      user = User.create(email: 'dumb@email.com', password: 'password')
+      expect(user.find_by_credentials('dumb@email.com', 'password')).to eq(user)
+    end
+  end
+end
